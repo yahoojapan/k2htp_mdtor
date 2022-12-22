8e273506175de1e6cc5c8003a39e143dc01ce6d7
Takeshi Nakatani <nakatani@yahoo-corp.jp>
2022-12-22 19:48:49 +0900

    Updated header and footer in comment lines

--- lib/k2htpmdtorlibs.h
+++ lib/k2htpmdtorlibs.h
@@ -59,7 +59,10 @@ typedef std::vector<MdtorTpLib*>	mdtortplibs_t;
 #endif	// K2HTPMDTORLIBS_H
 
 /*
- * VIM modelines
- *
- * vim:set ts=4 fenc=utf-8:
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noexpandtab sw=4 ts=4 fdm=marker
+ * vim<600: noexpandtab sw=4 ts=4
  */

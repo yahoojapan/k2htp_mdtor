71329f7e958ffd8dd1ec77b9c67f7926095860b4
Nakatani Takeshi <nakatani@yahoo-corp.jp>
2025-10-15 17:37:05 +0900

    Changed support OS and Updated codes/mail addresses

--- tests/test.sh
+++ tests/test.sh
@@ -87,7 +87,7 @@ PRNERR()
 	echo "${CBLD}${CRED}[ERROR]${CDEF} ${CRED}$*${CDEF}"
 }
 
-# shellcheck disable=SC2317
+# shellcheck disable=SC2317,SC2329
 PRNWARN()
 {
 	echo "${CYEL}${CREV}[WARNING]${CDEF} $*"
